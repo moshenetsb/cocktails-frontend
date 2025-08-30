c3bbef3f1d26b21da34f8410c3a183095fa803ba
moshenetsb <bogdan.moshenets@outlook.com>
2025-08-30 22:42:20 +0200

    Refactor routing for GitHub Pages support and enhance 404 redirection

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import "./styles/App.css";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
+import { useEffect } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
 import HomeScreen from "./screens/HomeScreen.js";
 import FavouritesScreen from "./screens/FavouritesScreen.js";
 import CocktailDetailsScreen from "./screens/CocktailDetailsScreen.js";
@@ -7,17 +9,29 @@ import NotFoundScreen from "./screens/NotFoundScreen.js";
 import { FavouritesProvider } from "./hooks/FavouritesContext.js";
 
 function App() {
+  //------------------------------------------
+  //For gh-pages
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const params = new URLSearchParams(location.search);
+    const redirect = params.get("redirect");
+    if (redirect) {
+      navigate(redirect, { replace: true });
+    }
+  }, [location, navigate]);
+  //------------------------------------------
+
   return (
     <div className="App">
       <FavouritesProvider>
-        <Router basename="/cocktails-frontend">
-          <Routes>
-            <Route path="/" element={<HomeScreen />} />
-            <Route path="/favourites" element={<FavouritesScreen />} />
-            <Route path="/cocktails/:id" element={<CocktailDetailsScreen />} />
-            <Route path="*" element={<NotFoundScreen />} />
-          </Routes>
-        </Router>
+        <Routes>
+          <Route path="/" element={<HomeScreen />} />
+          <Route path="/favourites" element={<FavouritesScreen />} />
+          <Route path="/cocktails/:id" element={<CocktailDetailsScreen />} />
+          <Route path="*" element={<NotFoundScreen />} />
+        </Routes>
       </FavouritesProvider>
     </div>
   );
